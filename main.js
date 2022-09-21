49fc14dd210c1c92095e4c777f78925798204998
Diegu <diegudeveloper@gmail.com>
2022-09-21 12:47:49 +0200

    Inicio de la configuracion del input progres

--- main.js
+++ main.js
@@ -3,11 +3,14 @@ const play = document.querySelector("#play")
 const pause = document.querySelector("#pause")
 const avanza = document.querySelector("#avanza")
 const retrocede = document.querySelector("#retrocede")
+const progres = document.querySelector("#progres")
 
 play.addEventListener('click', clickPlay);
 pause.addEventListener('click', clickPause);
 avanza.addEventListener('click', clickAvanza);
 retrocede.addEventListener('click', clickRetrocede);
+video.addEventListener('loadedmetadata', videoProgress);
+video.addEventListener('timeupdate', videoTimeUpdate);
 
 function clickPlay () {
     video.play();
@@ -29,3 +32,15 @@ function clickRetrocede () {
     video.currentTime = video.currentTime - 10
     
 }
+
+function videoProgress () {
+    progres.max = video.duration
+    console.log("vamos bien", video.duration);
+    
+}
+function videoTimeUpdate () {
+    
+    
+}
+
+ 
