0cfee5afc13a63c5a25484901510155663cc2b49
Diegu <diegudeveloper@gmail.com>
2022-09-21 10:43:17 +0200

    Inicio de la configuracion de los botones avanza y retrocede

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const video = document.querySelector(".video")
 const play = document.querySelector("#play")
 const pause = document.querySelector("#pause")
+const avanza = document.querySelector("#avanza")
+const retrocede = document.querySelector("#retrocede")
 
 play.addEventListener('click', clickPlay);
 pause.addEventListener('click', clickPause);
+avanza.addEventListener('click', clickAvanza);
+retrocede.addEventListener('click', clickRetrocede);
 
 function clickPlay () {
     video.play();
@@ -15,4 +19,13 @@ function clickPause () {
     video.pause();
     pause.hidden = true
     play.hidden = false
+}
+
+function clickAvanza () {
+    console.log("vamos bien");
+    
+}
+function clickRetrocede () {
+    console.log("vamos Superbien");
+    
 }
